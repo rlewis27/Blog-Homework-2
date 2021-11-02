65a715f84c5e94056c1f94df6416d36cfe11c10b
etorlic <emin.torlic@icloud.com>
2021-11-02 11:09:05 -0700

    Firebase Initialized

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgnT0Ta3uT2mqY6w-nyi8tZQmFPaUgDUc",
+  authDomain: "bloghomework2.firebaseapp.com",
+  projectId: "bloghomework2",
+  storageBucket: "bloghomework2.appspot.com",
+  messagingSenderId: "467751579800",
+  appId: "1:467751579800:web:b5f7c68c40400640f720ed",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
